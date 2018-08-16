301d5bbad2a9d6b399d04c72fda5873e948b6c59
dustyjay <janetarisah01@gmail.com>
2018-08-16 09:32:25 +0100

    fixes on home

---
+++ assets/js/home.js
@@ -0,0 +1,50 @@
+$(function(){
+    // $header = $('.header');
+    // $('')
+    // $header.css({
+    //     'position': 'fixed',
+    //     'z-index': '20'
+    // });
+
+    // $(window).on('scroll',function(){
+    //     $this = $(this);
+    //     if($this.scrollTop() > $this.height()){
+    //         $header.css({
+    //             'position': 'fixed',
+    //             'z-index': '20'
+    //         });
+    //     }
+    //     else $header.css('position','relative');
+    // });
+
+    $navbar = $(".navbar");
+    $screen = $(".container-fluid");
+    $sidebar = $(".sidebar-mini");
+    $close_sidebar = $("#close-side");
+
+    $navbar.click(function(){
+        $sidebar.css({
+            "width":"240px",
+            "padding": "15% 5%"
+        });
+        $screen.addClass('fade-out');
+    });
+
+    $close_sidebar.click(function(){
+        $sidebar.css({
+            "width":"0px",
+            "padding": "0px"
+        });
+        $screen.removeClass('fade-out');
+    });
+
+    $screen.click(function(){
+        if($sidebar.css("width") == "240px"){
+            $(this).removeClass("fade-out");
+            $sidebar.css({
+                "width":"0px",
+                "padding": "0px"
+            });
+        }
+    });
+});
