6317517b27e103fff329786d4ca6fa9289329306
dustyjay <janetarisah01@gmail.com>
2018-08-17 16:49:00 +0100

    fixed sliders

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ $(function(){
 
     $(".slides").slick({
         arrows: true,
-        autoplay: true,
+        // autoplay: true,
         autoplaySpeed: 7000,
         prevArrow: '<span class="prev-arrow"><img src="assets/img/left-arrow.svg"/></span>',
         nextArrow: '<span class="next-arrow"><img src="assets/img/right-arrow.svg"/></span>',
